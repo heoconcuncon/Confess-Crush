e2279d01061d3272b88cd6fe514576c93a62c0af
heoconcuncon <94803112+heoconcuncon@users.noreply.github.com>
2021-11-22 02:23:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "CHÚC MỪNG SINH NHẬT CHỒNG IU",
+    introTitle: 'Cún con của em',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnIntro: 'Hehe',
+    title: 'TUỔI MỚI PHẢI QUAN TÂM EM NHIỀU HƠN VÀ YÊU EM NHIỀU HƠN NỮA NHA',
+    desc: 'CẢM ƠN CHỒNG NHIỀU NHIỀU NHIỀU',
+    btnYes: 'YÊU BÉ',
+    btnNo: 'HÔNG IU BÉ :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao chồng em lại yêu em ta <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Bé biết mà 🥰. Yêu chồng nhiều nhiều 😘😘',
+    messDesc: 'Chúc chồng em sinh thần vui vẻ, mong thời gian sắp tới sẽ gặp đươc chồng',
+    btnAccept: 'Dạ bé',
+    messLink: 'https://github.com/heoconcuncon/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
